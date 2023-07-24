86215710951dd38535e2915b9d4cd022c9fecfea
Iron-E <code.iron.e@gmail.com>
2023-07-24 15:13:31 -0400

    feat(schema): Organization

--- src/schema/organization.ts
+++ src/schema/organization.ts
@@ -1,8 +1,11 @@
 import { Id } from "./id";
+import { Location } from "./location";
 
 /**
- * Same as {https://github.com/Iron-E/winvoice-schema | `Foo`} type.
+ * Same as {https://github.com/Iron-E/winvoice-schema | `Organization`} type.
  */
-export type Foo = {
+export type Organization = {
 	id: Id,
+	location: Location,
+	name: string,
 };

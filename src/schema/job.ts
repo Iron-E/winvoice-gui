d41c63dce0c1d8ea3db8551f1c346539456653c6
Iron-E <code.iron.e@gmail.com>
2023-07-24 15:41:23 -0400

    feat(schema): Job

--- src/schema/job.ts
+++ src/schema/job.ts
@@ -1,8 +1,25 @@
+import { Department } from "./department";
+import { Duration } from "./duration";
 import { Id } from "./id";
+import { Invoice } from "./invoice";
+import { Organization } from "./organization";
 
 /**
- * Same as {https://github.com/Iron-E/winvoice-schema | `Foo`} type.
+ * Same as {@link https://github.com/Iron-E/winvoice-schema | `Job`} type.
  */
-export type Foo = {
+export type Job = {
+	client: Organization,
+	date_close?: Date,
+	date_open: Date,
+	departments: Department[],
 	id: Id,
+	invoice: Invoice,
+	notes: string,
+	objectives: string,
+
+	/**
+	 * A concrete {@link Duration} or a {@link https://docs.rs/humantime/latest/humantime | human-readable duration}
+	 *
+	 */
+	increment: Duration | string,
 };

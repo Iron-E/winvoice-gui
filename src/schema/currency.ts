f555e0cbfe7119bfefa828bec08318de3f3672b0
Iron-E <code.iron.e@gmail.com>
2023-07-24 13:23:37 -0400

    ref: implement `Currency`

---
+++ src/schema/currency.ts
@@ -0,0 +1,47 @@
+/**
+ * Equivalent to the {@link https://docs.rs/money2/latest/money2/enum.Currency.html | Currency} type.
+ */
+export enum Currency
+{
+	Aud,
+	Bgn,
+	Brl,
+	Cad,
+	Chf,
+	Cny,
+	Czk,
+	Dkk,
+	Eur,
+	Gbp,
+	Hkd,
+	Huf,
+	Idr,
+	Ils,
+	Inr,
+	Isk,
+	Jpy,
+	Krw,
+	Mxn,
+	Myr,
+	Nok,
+	Nzd,
+	Php,
+	Pln,
+	Ron,
+	Rub,
+	Sek,
+	Sgd,
+	Thb,
+	Try,
+	Usd,
+	Zar,
+}
+
+export namespace Currency {
+	/**
+	 * @returns The default {@link Currency}.
+	 */
+	export function default_(): Currency {
+		return Currency.Eur;
+	}
+}

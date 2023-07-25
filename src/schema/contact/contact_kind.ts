94c2f53dd338222c4c0194050afa6a723e3a0136
Iron-E <code.iron.e@gmail.com>
2023-07-25 16:18:04 -0400

    ref(ContactKind): use `UnionToKeys`

--- src/schema/contact/contact_kind.ts
+++ src/schema/contact/contact_kind.ts
@@ -1,11 +1,10 @@
-import { Location } from "../location";
+import { Location } from '../location';
+import { UnionToKeys } from 'utils/union_to_keys';
 
 /**
  * Same as {@link https://github.com/Iron-E/winvoice-schema | `ContactKind`} type.
  */
 export type ContactKind =
 	| { address: Location }
-	| { email: string }
-	| { other: string }
-	| { phone: string }
-;
+	| UnionToKeys<'email' | 'other' | 'phone', string>
+	;

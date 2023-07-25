f0d533e9a72372a73cd4a1e1e2e8ed139cbb12e1
Iron-E <code.iron.e@gmail.com>
2023-07-25 16:18:44 -0400

    ref(Duration): simplify declaration

--- src/schema/duration.ts
+++ src/schema/duration.ts
@@ -1,7 +1,4 @@
 /**
  * Same as {@link https://doc.rust-lang.org/core/time/struct.Duration.html | `Duration`} type.
  */
-export type Duration = {
-	nanos: number,
-	secs: number,
-};
+export type Duration = { [key in 'nanos' | 'secs']: number };

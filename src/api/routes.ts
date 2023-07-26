20d7deb2ff0cc2f28665f83f062aae6c9daee765
Iron-E <code.iron.e@gmail.com>
2023-07-26 16:47:01 -0400

    feat(api): implement `routes`

---
+++ src/api/routes.ts
@@ -0,0 +1,38 @@
+/** Same as {@link https://github.com/Iron-E/winvoice-server | `api::routes::CONTACT`}. */
+export const CONTACT = '/contact';
+
+/** Same as {@link https://github.com/Iron-E/winvoice-server | `api::routes::DEPARTMENT`}. */
+export const DEPARTMENT = "/department";
+
+/** Same as {@link https://github.com/Iron-E/winvoice-server | `api::routes::EMPLOYEE`}. */
+export const EMPLOYEE = "/employee";
+
+/** Same as {@link https://github.com/Iron-E/winvoice-server | `api::routes::EXPENSE`}. */
+export const EXPENSE = "/expense";
+
+/** Same as {@link https://github.com/Iron-E/winvoice-server | `api::routes::EXPORT`}. */
+export const EXPORT = "/job/export";
+
+/** Same as {@link https://github.com/Iron-E/winvoice-server | `api::routes::JOB`}. */
+export const JOB = "/job";
+
+/** Same as {@link https://github.com/Iron-E/winvoice-server | `api::routes::LOCATION`}. */
+export const LOCATION = "/location";
+
+/** Same as {@link https://github.com/Iron-E/winvoice-server | `api::routes::LOGIN`}. */
+export const LOGIN = "/login";
+
+/** Same as {@link https://github.com/Iron-E/winvoice-server | `api::routes::LOGOUT`}. */
+export const LOGOUT = "/logout";
+
+/** Same as {@link https://github.com/Iron-E/winvoice-server | `api::routes::ORGANIZATION`}. */
+export const ORGANIZATION = "/organization";
+
+/** Same as {@link https://github.com/Iron-E/winvoice-server | `api::routes::ROLE`}. */
+export const ROLE = "/role";
+
+/** Same as {@link https://github.com/Iron-E/winvoice-server | `api::routes::TIMESHEET`}. */
+export const TIMESHEET = "/timesheet";
+
+/** Same as {@link https://github.com/Iron-E/winvoice-server | `api::routes::USER`}. */
+export const USER = "/user";

abbb66fa06162dea62b4ec7d293c26b467b89610
Iron-E <code.iron.e@gmail.com>
2023-07-26 15:12:38 -0400

    chore: ensure `type` annotation on `import`s

--- src/match/match_department.ts
+++ src/match/match_department.ts
@@ -1,6 +1,6 @@
 import { type Id } from "../schema";
 import { type Match } from "./match";
-import { MatchStr } from "./match_str";
+import { type MatchStr } from "./match_str";
 
 /** Same as {@link https://github.com/Iron-E/winvoice-match | `MatchDepartment`}. */
 export type MatchDepartment = {

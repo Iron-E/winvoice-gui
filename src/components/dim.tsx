67e0f1bddb5305b31c4d6d03571b531e87605322
Iron-E <code.iron.e@gmail.com>
2023-08-08 16:01:19 -0400

    fix(components): `Dim` opacity prop not working properly

--- src/components/dim.tsx
+++ src/components/dim.tsx
@@ -1,15 +1,12 @@
 import type { Children, Click } from "./props-with";
 
 /** The properties of a {@link Dim}. */
-export type Props = Children<Click<{
-	/** how much of the background page can show through the dim. Should be 0–1 (inclusive).  */
-	opacity: number,
-}>>;
+export type Props = Children<Click>;
 
 /** @return a floating `div` which dims the screen at `z-0`. */
 export function Dim(props: Props): React.ReactElement {
 	return (
-		<div className={`fixed top-0 left-0 w-screen h-screen bg-black/[${props.opacity}] z-1`} onClick={props.onClick}>
+		<div className={`fixed top-0 left-0 w-screen h-screen bg-black bg-opacity-70 z-1`} onClick={props.onClick}>
 			{props.children}
 		</div>
 	);
--- src/components/modal.tsx
+++ src/components/modal.tsx
@@ -12,7 +12,7 @@ export function Modal(props: Props): React.ReactElement {
 	hooks.useKeydownHandler({ Escape: true }, props.onClose);
 
 	return (
-		<Dim onClick={props.onClose} opacity={0.7}>
+		<Dim onClick={props.onClose}>
 			<div className='absolute top-1/2 -translate-y-1/2 left-1/2 -translate-x-1/2 rounded bg-gray-300' onClick={(e) => e.stopPropagation()}>
 				<XButton className='w-5 mt-1 mr-1 absolute top-0 right-0' onClick={props.onClose} />
 				<div className='my-3 mx-10'>

7541deb84df65584c15346dedbeb6e5bc26ae262
Iron-E <code.iron.e@gmail.com>
2023-08-10 14:40:48 -0400

    fix(Navbar): invalid links showing

--- src/components/header/navbar.tsx
+++ src/components/header/navbar.tsx
@@ -5,9 +5,25 @@ import Link from 'next/link';
 import type { ClassName } from '../props-with';
 import { Route } from '../../api';
 
+/** The CSS styles that all {@link Navbar} {@link Links} share. */
 const COMMON_LINK_STYLE = 'mx-1 px-2 py-1 border-2 rounded xl:hover:border-green-300 duration-200';
+
+/** The CSS style of the current {@link Link}. */
 const CURRENT_LINK_STYLE = `${COMMON_LINK_STYLE} bg-green-300 border-green-400 text-gray-950`;
+
+/** The CSS style of inactive {@link Link}s. */
 const INACTIVE_LINK_STYLE = `${COMMON_LINK_STYLE} border-transparent hover:border-green-400`;
+
+/** The {@link Route}s which are to be hidden in the {@link Navbar}. */
+const HIDDEN_LINKS: { [key in Route]?: true } = {
+	[Route.Export]: true,
+	[Route.Login]: true,
+	[Route.Logout]: true,
+	[Route.WhoAmI]: true,
+};
+
+/** The {@link Link}s which are to be shown in the {@link Navbar}. */
+const LINKS = Object.entries(Route).filter(([_, route]) => !HIDDEN_LINKS[route]);
 
 /** @return the navigation bar for the given `current` page. */
 export function NavBar(props: ClassName): React.ReactElement {
@@ -15,7 +31,7 @@ export function NavBar(props: ClassName): React.ReactElement {
 
 	return (
 		<nav className={props.className}>
-			{Object.entries(Route).map(([name, route]) => (
+			{LINKS.map(([name, route]) => (
 				<Link className={PATH_NAME == route ? CURRENT_LINK_STYLE : INACTIVE_LINK_STYLE} href={route} key={route}>
 					{name}
 				</Link>

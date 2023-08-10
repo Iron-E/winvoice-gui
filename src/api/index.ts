5844a584252a7b7eb3c01d06a1d4b80ee28b2b32
Iron-E <code.iron.e@gmail.com>
2023-08-10 18:26:30 -0400

    ref(api): make API header compliant with HTTP 3.0

--- src/api/index.ts
+++ src/api/index.ts
@@ -4,4 +4,4 @@ export { Route } from './route';
 export { Code, type Status } from './status';
 
 /** The request header used to indicate the expected API version on the {@link https://github.com/Iron-E/winvoice-sevrer | winvoice-server}. */
-export const VERSION_HEADER = { 'Api-Version': '^0.1' } as const;
+export const VERSION_HEADER = { 'api-version': '^0.1' } as const;

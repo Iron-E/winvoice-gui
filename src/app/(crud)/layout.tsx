7664693cbd79b6cc1349d97bf313082fcf054f81
Iron-E <code.iron.e@gmail.com>
2023-08-28 13:52:16 -0400

    ref(CrudLayout): center elements inside `<main>`

--- src/app/(crud)/layout.tsx
+++ src/app/(crud)/layout.tsx
@@ -1,9 +1,9 @@
-import { propsWith as w } from '@/components';
+import { css, propsWith as w } from '@/components';
 
 /** @return the layout used for all pages with CRUD logic in the application. */
 export default function CrudLayout(props: w.Children): React.ReactElement {
 	return (
-		<main>
+		<main className={`${css.FLEX} justify-center`}>
 			{props.children}
 		</main>
 	);

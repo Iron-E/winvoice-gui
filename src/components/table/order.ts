004cdfe7c00c453f470602023d7747a8755fac47
Iron-E <code.iron.e@gmail.com>
2023-09-08 16:36:09 -0400

    ref(Order): implement extractors
    This will allow sorting by an inner table (e.g. `Location` → `Outer` →
    `Name`)

--- src/components/table/order.ts
+++ src/components/table/order.ts
@@ -1,3 +1,4 @@
+import { Fn } from "@/utils";
 import React from "react";
 
 /** The order by which rows in a table are sorted. */
@@ -9,27 +10,54 @@ export type Order<T> = Readonly<{
 	ascending: boolean,
 }>;
 
+/**
+ * Extractors for the {@link useOrderedData}.
+ * For example, this enforces that for some type `{a: {b: string, c: number}}`, `a` must be mapped to either `b` or `c`.
+ */
+type Extractors<T> = {
+	[key in keyof T]?: (value: NonNullable<T[key]>) => any;
+};
+
 export type OrderedData<T> = Readonly<{
-	data: Readonly<{ get: readonly T[], set: (data: readonly T[]) => void }>,
-	order: Readonly<{ get: Order<keyof T>, set: (order: Order<keyof T>) => void }>,
+	data: Readonly<{ get: readonly T[], set?: Fn<[data: readonly T[], extractors?: Extractors<T>]> }>,
+	order: Readonly<{ get: Order<keyof T>, set: Fn<[order: Order<keyof T>, extractors?: Extractors<T>]> }>,
 }>;
 
 /**
  * @param defaultColumn the {@link Order.column | column} that is used to sort the data by default.
  * @return {@link Order<T>} (w/ {@link Order.ascending | `ascending === false`}) as {@link React.useState | state}.
  */
-export function useOrder<T>(defaultColumn: T): [Order<T>, React.Dispatch<React.SetStateAction<Order<T>>>] {
+export function useOrder<T>(defaultColumn: T): [Order<T>, Fn<[order: Order<T>]>] {
 	return React.useState<Order<T>>({ ascending: false, column: defaultColumn });
 }
 
-/** @return the `data` sorted according to the `order`. */
-export function orderData<T>(data: readonly T[], order: Order<keyof T>): readonly T[] {
+/**
+ * @param data what to sort
+ * @param order what to sort by
+ * @param extractors what to sort by when the `order` indicates that the {@link Order.column} is an {@link object}.
+ * @return the `data` sorted according to the `order`.
+ */
+export function orderData<T>(data: readonly T[], order: Order<keyof T>, extractors?: Extractors<T>): readonly T[] {
+	const extractor = extractors?.[order.column];
+
 	return [...data].sort((d1, d2) => {
-		if (d1[order.column] < d2[order.column]) {
+		let d1Value = d1[order.column];
+		let d2Value = d2[order.column];
+		if (extractor != undefined) {
+			if (d1Value != undefined) {
+				d1Value = extractor(d1Value);
+			}
+
+			if (d2Value != undefined) {
+				d2Value = extractor(d2Value);
+			}
+		}
+
+		if (d1Value < d2Value) {
 			var value = -1;
-		} else if (d1[order.column] > d2[order.column]) {
+		} else if (d1Value > d2Value) {
 			var value = 1;
-		} else {
+		} else /* the values are equal */ {
 			var value = 0;
 		}
 
@@ -42,20 +70,21 @@ export function orderData<T>(data: readonly T[], order: Order<keyof T>): readonl
  * @param defaultColumn the column which is used to sort the rows by default.
  * @return two sets of {@link React.useState}'s return: the first for the data, the second for the {@link Order}.
  */
-export function useOrderedData<T>(
-	defaultColumn: keyof T,
-): OrderedData<T> {
+export function useOrderedData<T>(defaultColumn: keyof T): OrderedData<T> & { data: Required<OrderedData<T>['data']> } {
 	const [DATA, setData] = React.useState<readonly T[]>([]);
 	const [ORDER, setOrder] = useOrder<keyof T>(defaultColumn);
 
 	return {
 		data: {
 			get: DATA,
-			set: d => setData(orderData(d, ORDER)),
+			set: (d, x) => setData(orderData(d, ORDER, x)),
 		},
 		order: {
 			get: ORDER,
-			set: o => { setData(orderData(DATA, o)); setOrder(o); },
+			set: (o, x) => {
+				setData(orderData(DATA, o, x));
+				setOrder(o);
+			},
 		},
 	};
 }

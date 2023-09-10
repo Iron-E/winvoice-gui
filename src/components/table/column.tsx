6289bbcf933e1a0c9d48f156fe6a7621cc1fb64b
Iron-E <code.iron.e@gmail.com>
2023-09-10 15:28:43 -0400

    feat(Td): allow disabling wrapping for specific column

--- src/components/table/column.tsx
+++ src/components/table/column.tsx
@@ -3,11 +3,12 @@ import { PAD } from "../css";
 
 /** The style of a {@link Td}. */
 export const COLUMN_STYLE = `${PAD} [&:not(:last-child)]:border-r-[1px] border-table-col-border` as const;
+const COLUMN_STYLE_NOWRAP = `${PAD} [&:not(:last-child)]:border-r-[1px] border-table-col-border whitespace-nowrap` as const;
 
 /** @return a `<td>` with the standard winvoice appearance. */
-export function Td(props: Children): React.ReactElement {
+export function Td(props: Children & { wrap?: boolean }): React.ReactElement {
 	return (
-		<td className={COLUMN_STYLE}>
+		<td className={props.wrap === false ? COLUMN_STYLE_NOWRAP : COLUMN_STYLE}>
 			{props.children}
 		</td>
 	);

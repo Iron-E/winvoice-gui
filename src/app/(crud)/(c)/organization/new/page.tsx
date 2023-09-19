4840aa3f9f06833936c21a58e95dfbc6dc620b9c
Iron-E <code.iron.e@gmail.com>
2023-09-19 15:24:13 -0400

    feat(c): New `Organization` Page

--- src/app/(crud)/(c)/organization/new/page.tsx
+++ src/app/(crud)/(c)/organization/new/page.tsx
@@ -1,6 +1,38 @@
+'use client';
+
+import React from 'react';
+import type { Organization } from '@/schema';
+import { OrganizationForm, OrganizationTable, organizationValuators, useLocationOrder, useOrderedData } from '@/components';
+
 export default function Page(): React.ReactElement {
-	return (
-		<>
-		</>
-	);
+	const [LOCATION_ORDER, setLocationOrder] = useLocationOrder();
+	const [OUTER_ORDER, setOuterOrder] = useLocationOrder();
+	const ORDERED_DATA = useOrderedData<Organization>('name', organizationValuators(
+		LOCATION_ORDER.column,
+		OUTER_ORDER.column,
+	));
+
+	return <>
+		<OrganizationForm
+			id='new-location-form'
+			// WARN: passing function (i.e. not using closure) changes 'this' context
+			onSubmit={o => ORDERED_DATA.append(o)}
+		/>
+
+		{ORDERED_DATA.data.length > 0 && (
+			<OrganizationTable
+				locationOrder={LOCATION_ORDER}
+				onReorderLocation={order => {
+					setLocationOrder(order);
+					ORDERED_DATA.refresh(organizationValuators(order.column, OUTER_ORDER.column));
+				}}
+				onReorderOuterLocation={order => {
+					setOuterOrder(order);
+					ORDERED_DATA.refresh(organizationValuators(LOCATION_ORDER.column, order.column));
+				}}
+				orderedData={ORDERED_DATA}
+				outerLocationOrder={LOCATION_ORDER}
+			/>
+		)}
+	</>;
 }

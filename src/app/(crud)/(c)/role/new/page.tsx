f7eec8905343ebc0c14f37121d41caa5b0e1bde8
Iron-E <code.iron.e@gmail.com>
2023-09-20 17:20:12 -0400

    feat(c): new role page

--- src/app/(crud)/(c)/role/new/page.tsx
+++ src/app/(crud)/(c)/role/new/page.tsx
@@ -1,6 +1,21 @@
+'use client';
+
+import React from 'react';
+import type { Role } from '@/schema';
+import { RoleForm, RoleTable, useOrderedData } from '@/components';
+
 export default function Page(): React.ReactElement {
-	return (
-		<>
-		</>
-	);
+	const ORDERED_DATA = useOrderedData<Role>('name');
+
+	return <>
+		<RoleForm
+			id='new-role-form'
+			// WARN: passing function (i.e. not using closure) changes 'this' context
+			onSubmit={d => ORDERED_DATA.append(d)}
+		/>
+
+		{ORDERED_DATA.data.length > 0 && (
+			<RoleTable orderedData={ORDERED_DATA} />
+		)}
+	</>;
 }

1f5f3481af8d8ed424c91418efcc14a16ccc6b41
Iron-E <code.iron.e@gmail.com>
2023-09-20 20:23:59 -0400

    feat(InputField): accept singular forms of duration units

--- src/components/form/field/duration.tsx
+++ src/components/form/field/duration.tsx
@@ -9,7 +9,7 @@ export function InputDuration(props: InputProps<string>): React.ReactElement {
 			id={props.id}
 			label={props.label ?? 'Duration'}
 			onChange={props.onChange}
-			pattern='(\d+\s*(m(in(utes)?)?|h(r|ours)?|d(ays)?|w(eeks)?|months|y(ears)?)\s*)+'
+			pattern='(\d+\s*(m(in(utes?)?)?|h(r|ours?)?|d(ays?)?|w(eeks?)?|months?|y(ears?)?)\s*)+'
 			required={props.required}
 			title={`${props.title}. e.g. "1y 2months 5w 3d 10h 40m"`}
 			type='text'

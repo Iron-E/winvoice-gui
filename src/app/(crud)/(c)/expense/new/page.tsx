22c02e37cd5a1cd7ffed26ed3a472a4515a6caad
Iron-E <code.iron.e@gmail.com>
2023-10-03 18:34:14 -0400

    feat(c): `NewExpensePage`

--- src/app/(crud)/(c)/expense/new/page.tsx
+++ src/app/(crud)/(c)/expense/new/page.tsx
@@ -1,6 +1,18 @@
+'use client';
+
+import React from 'react';
+import type { Expense } from '@/schema';
+import { ExpenseForm, ExpenseTable, expenseValuators, useOrderedData } from '@/components';
+
 export default function Page(): React.ReactElement {
-	return (
-		<>
-		</>
-	);
+	const ORDERED_DATA = useOrderedData<Expense>('category', expenseValuators());
+	return <>
+		<ExpenseForm
+			id='new-expense-form'
+			// WARN: passing function (i.e. not using closure) changes 'this' context
+			onSubmit={x => ORDERED_DATA.append(x)}
+		/>
+
+		{ORDERED_DATA.data.length > 0 && <ExpenseTable orderedData={ORDERED_DATA} />}
+	</>;
 }

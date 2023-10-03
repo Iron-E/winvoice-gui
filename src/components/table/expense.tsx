f7c1b97d46dbf86442d0d76341d2bb1247c633ce
Iron-E <code.iron.e@gmail.com>
2023-10-03 17:47:07 -0400

    feat(table): `ExpenseTable`

---
+++ src/components/table/expense.tsx
@@ -0,0 +1,58 @@
+'use client';
+
+import React from 'react';
+import type { BaseProps } from './props';
+import { moneyToString, type Expense } from '@/schema'
+import {
+	Table,
+	Td,
+	Tr,
+	useRowEventHandlers,
+} from '../table';
+import { getId } from '@/utils';
+import { ExpenseForm } from '../form';
+import { Route } from '@/api';
+import { useApiContext } from '../api';
+
+export * from './expense/hooks';
+export * from './expense/valuators';
+
+/** the headers of the {@link ExpenseTable}. */
+const HEADERS = ['Timesheet ID', 'ID', 'Category', 'Cost', 'Description'] as const;
+
+/** @returns a table which displays {@link Expense}s in a customizable manner. */
+export function ExpenseTable(props: BaseProps<Expense, 'id'>): React.ReactElement {
+	const [CLIENT, showMessage] = useApiContext();
+	const [HANDLER, setRowEvent] = useRowEventHandlers(
+		props.orderedData, CLIENT, showMessage, Route.Expense,
+		e => `expense ${e.id}`,
+		getId,
+		props => <ExpenseForm  {...props} id='edit-expense-form' />,
+	);
+
+	return <>
+		<Table
+			headers={HEADERS}
+			onReorder={props.orderedData.setOrder}
+			order={props.orderedData.order}
+		>
+			{props.orderedData.data.map(x => (
+				<Tr
+					key={x.id}
+					onClick={props.onRowSelect && (() => props.onRowSelect!(x))}
+					onDelete={props.deletable !== false ? () => setRowEvent({ action: 'delete', data: x }) : undefined}
+					onEdit={() => setRowEvent({ action: 'edit', data: x })}
+					selected={x.id === props.selectedRow}
+				>
+					<Td>{x.timesheet_id}</Td>
+					<Td>{x.id}</Td>
+					<Td>{x.category}</Td>
+					<Td>{moneyToString(x.cost)}</Td>
+					<Td>{x.description}</Td>
+				</Tr>
+			))}
+		</Table>
+
+		{HANDLER}
+	</>;
+}

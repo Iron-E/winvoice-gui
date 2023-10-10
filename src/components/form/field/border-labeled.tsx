8f05814ab286183887f7e991c0fb9bc3864f41f7
Iron-E <code.iron.e@gmail.com>
2023-10-10 16:35:57 -0400

    fix(BorderLabeledField): alternating nest color stops after certain depth
    This had to be solved with JS rather than CSS :/

--- src/components/form/field/border-labeled.tsx
+++ src/components/form/field/border-labeled.tsx
@@ -1,3 +1,6 @@
+'use client';
+
+import React from "react";
 import type { Children, Class, Click } from "../../props-with";
 import type { Props, ValueOf } from "@/utils";
 import { BorderedLabel, FormButton } from "@/components";
@@ -5,6 +8,9 @@ import { HOVER } from "@/components/css";
 
 /** Style to allow the border-labeled field to show as a 2×2 grid. */
 export const GRID = 'grid grid-rows-[1.5rem_1fr] grid-flow-col gap-x-2' as const;
+
+/** `true` if even, `false` if odd. */
+const NESTING_CONTEXT = React.createContext(false);
 
 /**
  * @returns a {@link React.JSX.IntrinsicElements.form | form} which will either create a new {@link InvoiceDate} on submit (if `intialValues` is `undefined`), or simply call `onSubmit` with the result of the changes to the `initialValues` otherwise (to allow editing data).
@@ -17,13 +23,13 @@ export function BorderLabeledField(props:
 		label?: ValueOf<Props<typeof BorderedLabel>, 'label'>,
 	}
 ): React.ReactElement {
+	const NESTING = React.useContext(NESTING_CONTEXT);
 	return (
 		<BorderedLabel
+			// GIGA-HACK: there is no way to do even/odd nesting in css!
 			className={`flex flex-col justify-between \
 min-h-[1.5rem] left-1 mt-4 mb-2 pt-4 pl-1 pr-3 \
-bg-bordered-label-bg
-[&>&]:bg-bordered-label-nested-bg
-[&>&>&]:bg-bordered-label-bg
+${NESTING ? 'bg-bordered-label-nested-bg' : 'bg-bordered-label-bg'}
 ${props.className}`}
 			label={props.label}
 		>
@@ -36,7 +42,9 @@ ${props.className}`}
 				</FormButton>
 			)}
 
-			{props.children}
+			<NESTING_CONTEXT.Provider value={!NESTING}>
+				{props.children}
+			</NESTING_CONTEXT.Provider>
 		</BorderedLabel>
 	);
 }

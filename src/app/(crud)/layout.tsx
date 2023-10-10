98ac95ba9965d7cf07f7a1c22acb01d19923bb43
Iron-E <code.iron.e@gmail.com>
2023-10-10 16:36:38 -0400

    fix(CrudLayout): header doesn't go full page width when zooming

--- src/app/(crud)/layout.tsx
+++ src/app/(crud)/layout.tsx
@@ -4,7 +4,7 @@ import { InnerCrudLayout } from './layout/inner';
 /** @returns the layout used for all pages with CRUD logic in the application. */
 export default function CrudLayout(props: w.Children): React.ReactElement {
 	return (
-		<main className={`${css.FLEX} flex-col justify-center gap-4`}>
+		<main className={`${css.FLEX} flex-col justify-center gap-4 overflow-x-scroll`}>
 			<InnerCrudLayout />
 			{props.children}
 		</main>

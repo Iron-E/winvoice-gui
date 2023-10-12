a6b0ea6db0a14aa234a7da2019f047467136d55b
Iron-E <code.iron.e@gmail.com>
2023-10-12 19:17:37 -0400

    feat(MatchDepartmentForm): POST requests populating table

--- src/components/form/match/department.tsx
+++ src/components/form/match/department.tsx
@@ -3,21 +3,32 @@
 import React from 'react';
 import type { MatchDepartment } from '@/match'
 import type { SearchProps } from './props';
+import { DepartmentTable } from '@/components/table/department';
 import { Form, FormButton } from '../../form';
 import { InputMatchDepartment } from '../field';
 import { Route } from '@/api';
+import { SPACE } from '@/components/css';
 import { type Department, isDepartment } from '@/schema';
 import { useApiContext } from '../../api';
-import { SPACE } from '@/components/css';
+import { useOrderedData } from '@/components/table/order/data';
 
 /**
  * @returns a {@link React.JSX.IntrinsicElements.form | form} which will either create a new {@link MatchDepartment} on submit (if `intialValues` is `undefined`), or simply call `onSubmit` with the result of the changes to the `initialValues` otherwise (to allow editing data).
  */
 export function MatchDepartmentForm(props: SearchProps<Department>): React.ReactElement {
+	const [CLIENT, showMessage] = useApiContext();
 	const [MATCH, setMatch] = React.useState<MatchDepartment>({});
+	const ORDERED_DATA = useOrderedData<Department>('name');
+
 	return <>
 		<Form
 			onSubmit={async () => {
+				const RESULT = await CLIENT.retrieve(showMessage, Route.Department, { condition: MATCH }, isDepartment);
+				if (RESULT === null) { return; } else if (RESULT.length < 1) {
+					showMessage('info', 'Search returned no results');
+				}
+
+				ORDERED_DATA.setData?.(RESULT);
 			}}
 		>
 			<InputMatchDepartment
@@ -29,5 +40,7 @@ export function MatchDepartmentForm(props: SearchProps<Department>): React.React
 
 			<FormButton className={SPACE} />
 		</Form>
+
+		{ORDERED_DATA.data.length > 0 && <DepartmentTable orderedData={ORDERED_DATA} />}
 	</>;
 }

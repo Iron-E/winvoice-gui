a5e87b8d4aecaea2334fb343e4950eeb94335541
Iron-E <code.iron.e@gmail.com>
2023-10-12 19:18:03 -0400

    feat(rud): `DepartmentSearchPage`

--- src/app/(crud)/(rud)/department/page.tsx
+++ src/app/(crud)/(rud)/department/page.tsx
@@ -1,12 +1,5 @@
-'use client';
-
-import React from "react";
 import { MatchDepartmentForm } from "@/components";
 
 export default function Page(): React.ReactElement {
-	return (
-		<MatchDepartmentForm
-			id='test-input-match-department'
-		/>
-	);
+	return <MatchDepartmentForm id='test-input-match-department' />;
 }

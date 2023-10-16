83d0baed70edb70aabe99641f3c9b99822ce0450
Iron-E <code.iron.e@gmail.com>
2023-10-16 14:26:56 -0400

    feat(field-match): `InputMatchBool`

---
+++ src/components/form/field/match/bool.tsx
@@ -0,0 +1,18 @@
+'use client';
+
+import type { Match } from '@/match';
+import { Checkbox } from '../../field';
+import { InputMatch, InputMatchProps, type InputMatchField } from '../match';
+
+const InputBool: InputMatchField<boolean> = props => (
+	<Checkbox {...props} inputClassName='min-w-[36ch]'>
+		{props.label ?? 'Active'}
+	</Checkbox>
+);
+
+/**
+ * @returns a {@link React.JSX.IntrinsicElements.form | form} which will either create a new {@link MatchId} on submit (if `intialValues` is `undefined`), or simply call `onSubmit` with the result of the changes to the `initialValues` otherwise (to allow editing data).
+ */
+export function InputMatchBool(props: InputMatchProps<Match<boolean>>): React.ReactElement {
+	return <InputMatch {...props} inputField={InputBool} onChange={value => props.onChange(value ?? false)} />;
+}

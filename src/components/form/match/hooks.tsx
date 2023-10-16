9d5eb675499a7711dae28761ade8479d8f406fa3
Iron-E <code.iron.e@gmail.com>
2023-10-16 14:45:46 -0400

    ref: extract search forms to `useMatchForm`

---
+++ src/components/form/match/hooks.tsx
@@ -0,0 +1,47 @@
+'use client';
+
+import React from 'react';
+import type { InputMatchObjectProps } from '../field/match/props';
+import type { ReadonlyNonNullUnitArray, ValueOf } from '@/utils';
+import type { UserInputRoute } from '@/api';
+import type { UseTable } from '@/components/table';
+import { Form, FormButton } from '../../form';
+import { SPACE } from '@/components/css';
+import { useApiContext } from '../../api';
+
+/**
+ * @param <M> the type which is used to match `<T>`
+ * @param <T> the type being matched
+ * @param id the HTML `id` of this form.
+ * @param Input the fields in the form.
+ * @param useTable a function which will {@link UseTable<T>}
+ * @param route the API endpoint to perform the search on.
+ * @param checkSchema a function to determine if the result of the {@link fetch} returned an expected result.
+ */
+export function useMatchForm<T extends {}, M extends {}>(
+	id: ValueOf<InputMatchObjectProps<M>, 'id'>,
+	Input: (props: InputMatchObjectProps<M>) => React.ReactElement,
+	useTable: () => UseTable<T>,
+	route: UserInputRoute,
+	checkSchema: (json: unknown) => json is T,
+): React.ReactElement {
+	const [CLIENT, showMessage] = useApiContext();
+	const [MATCH, setMatch] = React.useState({} as M);
+	const [ORDERED_DATA, TABLE] = useTable();
+	return <>
+		<Form
+			onSubmit={async () => {
+				const RESULT = await CLIENT.retrieve(showMessage, route, { condition: MATCH }, checkSchema);
+				if (RESULT === null) { return; } else if (RESULT.length < 1) {
+					showMessage('info', 'Search returned no results');
+				}
+
+				ORDERED_DATA.setData?.(RESULT as unknown as ReadonlyNonNullUnitArray<T>);
+			}}
+		>
+			<Input id={`${id}--employee`} onChange={setMatch} value={MATCH} />
+			<FormButton className={SPACE} />
+		</Form>
+		{TABLE}
+	</>;
+}

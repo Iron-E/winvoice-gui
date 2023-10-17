8a170b86b2637ad009828b0645d8bd4f107a44d5
Iron-E <code.iron.e@gmail.com>
2023-10-17 18:48:06 -0400

    feat(InputMatchDate): `InputMatchOptionDate`

--- src/components/form/field/match/date.tsx
+++ src/components/form/field/match/date.tsx
@@ -1,12 +1,19 @@
 'use client';
 
-import type { Match } from '@/match';
+import type { Match, MatchOption } from '@/match';
 import { InputDate } from '../../field';
-import { InputMatch, InputMatchProps } from '../match';
+import { InputMatch, InputMatchOption, InputMatchProps } from '../match';
 
 /**
  * @returns a {@link React.JSX.IntrinsicElements.form | form} which will either create a new {@link MatchDate} on submit (if `intialValues` is `undefined`), or simply call `onSubmit` with the result of the changes to the `initialValues` otherwise (to allow editing data).
  */
 export function InputMatchDate(props: InputMatchProps<Match<Date>>): React.ReactElement {
 	return <InputMatch {...props} defaultValue={undefined as unknown as Date} inputField={InputDate} />;
+}
+
+/**
+ * @returns a {@link React.JSX.IntrinsicElements.form | form} which will either create a new {@link MatchDate} on submit (if `intialValues` is `undefined`), or simply call `onSubmit` with the result of the changes to the `initialValues` otherwise (to allow editing data).
+ */
+export function InputMatchOptionDate(props: InputMatchProps<MatchOption<Match<Date>>>): React.ReactElement {
+	return <InputMatchOption {...props} inputField={InputMatchDate} />;
 }

b525eb98943e26b02a3dc210bcac26149c499727
Iron-E <code.iron.e@gmail.com>
2023-10-17 18:20:17 -0400

    feat(form-field-match): `InputMatchDuration`

---
+++ src/components/form/field/match/duration.tsx
@@ -0,0 +1,12 @@
+'use client';
+
+import type { Match } from '@/match';
+import { InputDuration } from '../../field';
+import { InputMatch, InputMatchProps } from '../match';
+
+/**
+ * @returns a {@link React.JSX.IntrinsicElements.form | form} which will either create a new {@link MatchId} on submit (if `intialValues` is `undefined`), or simply call `onSubmit` with the result of the changes to the `initialValues` otherwise (to allow editing data).
+ */
+export function InputMatchDuration(props: InputMatchProps<Match<string>>): React.ReactElement {
+	return <InputMatch {...props} defaultValue='' inputField={InputDuration} />;
+}

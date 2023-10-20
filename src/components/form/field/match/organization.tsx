17a324cabaf8f9c596913c63f09658256bd9bd76
Iron-E <code.iron.e@gmail.com>
2023-10-20 15:01:18 -0400

    fix(InputMatchOrganization): missing border around location fields

--- src/components/form/field/match/organization.tsx
+++ src/components/form/field/match/organization.tsx
@@ -3,6 +3,7 @@ import type { InputMatchObjectProps } from './props';
 import type { MatchOrganization } from '@/match';
 import { InputMatchIdAndName } from './id-name';
 import { InputMatchLocation } from './location';
+import { BorderLabeledField } from '../border-labeled';
 
 /**
  * @returns a {@link React.JSX.IntrinsicElements.form | form} which will either create a new {@link MatchOrganization} on submit (if `intialValues` is `undefined`), or simply call `onSubmit` with the result of the changes to the `initialValues` otherwise (to allow editing data).
@@ -11,10 +12,12 @@ export function InputMatchOrganization(props: InputMatchObjectProps<MatchOrganiz
 	const VALUE = props.value ?? {};
 	return <>
 		<InputMatchIdAndName id={props.id} onChange={props.onChange} value={VALUE} />
-		<InputMatchLocation
-			id={`${props.id}--location`}
-			onChange={location => props.onChange({ ...VALUE, location })}
-			value={VALUE.location}
-		/>
+		<BorderLabeledField label='Location'>
+			<InputMatchLocation
+				id={`${props.id}--location`}
+				onChange={location => props.onChange({ ...VALUE, location })}
+				value={VALUE.location}
+			/>
+		</BorderLabeledField>
 	</>;
 }

9b30c76c030404b3e34d163c6f54b8236995cc08
Iron-E <code.iron.e@gmail.com>
2024-04-30 14:58:23 -0400

    fix(JsonFields): incorrect property filtering

--- src/utils/json-fields.ts
+++ src/utils/json-fields.ts
@@ -1,4 +1,4 @@
 /** Retrieve the fields which will be serialized by {@link JSON.stringify}. */
 export type JsonFields<T extends {}> = {
-	[F in keyof T]: T[F] extends Function ? never : T[F];
+	[K in keyof T as T[K] extends Function ? never : K]: T[K];
 };

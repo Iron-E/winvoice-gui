f320648b7e9639e547047340fbf6e13d635c570c
Iron-E <code.iron.e@gmail.com>
2024-05-06 15:35:35 -0400

    build(npm): allow offline build

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,22 @@
 import './globals.css';
-import { InnerRootLayout } from './layout/inner';
-import { Inter } from 'next/font/google';
-import { propsWith as w } from '@/components';
+import localFont from 'next/font/local';
 import type { Metadata } from 'next';
+import { InnerRootLayout } from './layout/inner';
+import { propsWith as w } from '@/components';
 
-const INTER = Inter({ subsets: ['latin'] });
+const INTER = localFont({
+	src: [
+		{ path: '../static/font/inter/normal/100.woff2', weight: '100', style: 'normal' },
+		{ path: '../static/font/inter/normal/200.woff2', weight: '200', style: 'normal' },
+		{ path: '../static/font/inter/normal/300.woff2', weight: '300', style: 'normal' },
+		{ path: '../static/font/inter/normal/400.woff2', weight: '400', style: 'normal' },
+		{ path: '../static/font/inter/normal/500.woff2', weight: '500', style: 'normal' },
+		{ path: '../static/font/inter/normal/600.woff2', weight: '600', style: 'normal' },
+		{ path: '../static/font/inter/normal/700.woff2', weight: '700', style: 'normal' },
+		{ path: '../static/font/inter/normal/800.woff2', weight: '800', style: 'normal' },
+		{ path: '../static/font/inter/normal/900.woff2', weight: '900', style: 'normal' },
+	],
+});
 
 /** WARN: *must* be `metadata`, cannot be `METADATA` as per project style guidelines */
 export const metadata: Readonly<Metadata> = {

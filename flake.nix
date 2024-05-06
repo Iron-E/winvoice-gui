9b01a666468f7e03fd40eb3d732953714d0115a9
Iron-E <code.iron.e@gmail.com>
2024-05-06 15:36:39 -0400

    build(flake): `winvoice-gui`

--- flake.nix
+++ flake.nix
@@ -3,47 +3,127 @@
 
 	inputs = {
 		nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-		fenix = {
-			url = "github:nix-community/fenix";
-			inputs.nixpkgs.follows = "nixpkgs";
-		};
 	};
 
 	outputs = inputs @ { self, nixpkgs, ... }:
 	let
 		inherit (nixpkgs) lib;
 		inherit (self) outputs;
-		genSystems = lib.genAttrs ["aarch64-darwin" "aarch64-linux" "i686-linux" "x86_64-darwin" "x86_64-linux"];
 
+		name = "winvoice-gui";
+		version = "0.2.0";
+
+		genSystems = lib.genAttrs ["aarch64-darwin" "aarch64-linux" "i686-linux" "x86_64-darwin" "x86_64-linux"];
 		mkPkgs = system: import nixpkgs {
 			inherit system;
 			config.allowUnfree = true;
-			overlays = (builtins.attrValues outputs.overlays) ++ [
-				inputs.fenix.overlays.default
-			];
+			overlays = [ ];
 		};
+
+		nodeJs = builtins.getAttr "nodejs_21";
 	in {
 		devShells = genSystems (system:
 		let pkgs = mkPkgs system;
 		in {
 			default = pkgs.mkShell {
-				name = "winvoice-gui";
+				inherit name;
 
-				packages = with pkgs; [
-					# build
-					nodejs_21
-
+				packages = [(nodeJs pkgs)] ++ (with pkgs; [
 					# test
 					mkcert
 
 					# deploy
-					kind
-					kubectl
-					kubectl-cnpg
-					kubectx
+					kind kubectl kubectl-cnpg kubectx
 					terraform
-				];
+				]);
 			};
+		});
+
+		packages = genSystems (system:
+		let
+			inherit (lib) fileset;
+
+			pkgs = mkPkgs system;
+			inherit (pkgs) importNpmLock;
+
+			mkFileset = fileset: lib.fileset.toSource {
+				inherit fileset;
+				root = ./.;
+			};
+
+			nodejs = nodeJs pkgs;
+			packageJson = fileset.unions [ ./package-lock.json ./package.json ];
+			meta = {
+				description = "A web UI for Winvoice";
+				homepage = "https://github.com/Iron-E/winvoice-gui";
+				license = with lib.licenses; [gpl3Plus];
+				mainProgram = "winvoice-gui";
+			};
+
+			# see nixos.org/manual/nixpkgs/unstable#javascript-buildNpmPackage
+			winvoice-gui-unwrapped = pkgs.buildNpmPackage {
+				inherit version;
+				pname = "${name}-unwrapped";
+
+				# env vars
+				NEXT_TELEMETRY_DISABLED = 1;
+				NODE_ENV = "production";
+
+				# source
+
+				inherit nodejs;
+				npmConfigHook = importNpmLock.npmConfigHook;
+				npmDeps = importNpmLock {
+					npmRoot = mkFileset packageJson;
+				};
+
+				src = mkFileset (fileset.unions [
+					./jest.config.json
+					./next-env.d.ts
+					./next.config.js
+					./postcss.config.js
+					./src
+					./tailwind.config.js
+					./tests
+					./tsconfig-base.json
+					./tsconfig.json
+					packageJson
+				]);
+
+				postInstall = /* bash */ ''
+					# install locations
+					targetBin="$out/bin/${name}"
+					targetLib="$out/lib/node_modules/${name}"
+					tmpLib="$${targetLib}-2"
+
+					# grab next standalone files
+					cp -r "$targetLib/.next/standalone" "$tmpLib"
+					mkdir -p "$tmpLib/.next"
+					cp -r "$targetLib/.next/static" "$tmpLib/.next/static"
+
+					# move next standalone files into target location
+					rm -r "$targetLib"
+					mv "$tmpLib" "$targetLib"
+				'';
+
+				meta = {
+					inherit (meta) homepage license;
+					description = "${meta.description} (unwrapped)";
+				};
+			};
+
+			winvoice-gui = pkgs.writeShellApplication {
+				inherit meta name;
+
+				derivationArgs = { inherit version; };
+				runtimeEnv.NODE_ENV = "production";
+				runtimeInputs = [ nodejs ];
+
+				text = /* sh */ ''node "${lib.getLib winvoice-gui-unwrapped}/lib/node_modules/${name}/server.js"'';
+			};
+		in {
+			inherit winvoice-gui winvoice-gui-unwrapped;
+			default = winvoice-gui;
 		});
 
 		overlays = { };
